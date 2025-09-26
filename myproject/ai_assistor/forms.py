db3e7fbfe6a90585ff52da79811175a6aaac7017
Hiro-Huang <hirosykira@163.com>
2025-09-26 13:22:02 +0800

    删除文件

--- myproject/ai_assistor/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UploadHomeworkForm(forms.Form):
-    homework_image = forms.ImageField(label='上传作业图片')
-    student_id = forms.CharField(label='学号', max_length=100)
